e18a1473db96ddd0a209d3e8bd7f9def20eff436
Bright Djogatse <125486553+Dblay112@users.noreply.github.com>
2024-04-03 22:06:05 +0000

    Update 3-lru_cache.py

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""LLRU cache modulule"""
+"""LRU cache module"""
+
 from collections import OrderedDict
 
 BaseCaching = __import__('base_caching').BaseCaching
@@ -7,25 +8,28 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 class LRUCache(BaseCaching):
     """LRU class"""
+
     def __init__(self):
         super().__init__()
         self.lru = OrderedDict()
-    
+
     def put(self, key, item):
-        """method to insert key"""
+        """Method to insert a key-value pair."""
         if key is None or item is None:
             return
+
         self.cache_data[key] = item
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            print("DISCARD: {}".format(self.lru[0]))
-            del self.cache_data[self.lru[0]]
-            self.cache_data.popitem(last=False)
+            print("DISCARD: {}".format(self.lru.popitem(False)[0]))
+            del self.cache_data[self.lru.popitem(False)[0]]
+
         try:
             self.lru.remove(key)
         except ValueError:
             pass
+
         self.lru.append(key)
-        
-    def get(self,key):
-        """method to get key"""
+
+    def get(self, key):
+        """Method to get a value from the cache."""
         return self.cache_data.get(key)
