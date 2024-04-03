acd0c6d302e3fe3a2d9626da2ca193b84024d4fe
Bright Djogatse <125486553+Dblay112@users.noreply.github.com>
2024-04-03 13:57:16 +0000

    Update 0-basic_cache.py

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -9,10 +9,10 @@ class BasicCache(BaseCaching):
         """method to add an item to the cache"""
         if key is None or item is None:
             return
-        self.cache_data[key] = item
+        return self.cache_data[key] = item
 
     def get(self, key):
         """method to retrieve an item from the cache"""
         if key is None or key not in self.cache_data:
             return None
-        self.cache_data.get[key]
+        return self.cache_data.get[key]
