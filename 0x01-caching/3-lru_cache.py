e5b3d722549a5a7f5531cd06527841ecb5b99f89
Bright Djogatse <125486553+Dblay112@users.noreply.github.com>
2024-04-03 22:13:14 +0000

    Update 3-lru_cache.py

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,35 +1,184 @@
 #!/usr/bin/env python3
-"""LRU cache module"""
-
-from collections import OrderedDict
-
+"""
+LIFOCache module
+"""
+from collections import deque
 BaseCaching = __import__('base_caching').BaseCaching
 
 
-class LRUCache(BaseCaching):
-    """LRU class"""
+class Node:
+    """
+    Node Class
+    """
+    def __init__(self, data):
+        self.data = data
+        self.prev = None
+        self.next = None
 
+
+class List:
+    """
+    method (Doubly Linked list)
+    """
     def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_front(self, value):
+        """
+        method to insert data in the front of the list
+        """
+        new_node = Node(value)
+        new_node.next = self.head
+
+        if self.head is not None:
+            self.head.prev = new_node
+
+        else:
+            self.tail = new_node
+
+        self.head = new_node
+
+    def insert_end(self, value):
+        """
+        method to insert data at the end of the list
+        """
+        new_node = Node(value)
+        new_node.prev = self.tail
+
+        if self.tail is not None:
+            self.tail.next = new_node
+
+        else:
+            self.head = new_node
+
+        self.tail = new_node
+
+    def pop_front(self):
+        """
+        method to remove data from the front of the list
+        """
+        if self.head is None:
+            return
+
+        next = self.head.next
+
+        if next is not None:
+            next.prev = None
+        else:
+            self.tail = None
+
+        self.head = next
+
+    def pop_back(self):
+        """
+        Remove data from the back of the list
+        """
+        if self.tail is None:
+            return None
+
+        prev = self.tail.prev
+
+        if prev is not None:
+            prev.next = None
+        else:
+            self.head = None
+
+        self.tail = prev
+
+    def front(self):
+        """
+        Return the head of the list
+        """
+        return self.head
+
+    def back(self):
+        """
+        Returns the tail of the list
+        """
+        return self.tail
+
+    def erase(self, node):
+        """
+        remove a node from the list
+        """
+        if node is None:
+            return
+
+        prev = node.prev
+        next = node.next
+
+        if prev is not None:
+            prev.next = next
+
+        else:
+            self.head = next
+
+        if next is not None:
+            next.prev = prev
+        else:
+            self.tail = prev
+
+
+class LRUCache(BaseCaching):
+    """
+    LRU based caching system
+    """
+    def __init__(self):
+        """
+        Initialization of LRUCache class"""
+        """
         super().__init__()
-        self.lru = OrderedDict()
+        self.order = List()
+
+    def print_cache(self):
+        """ Print the cache
+        """
+        print("Current cache:")
+        for key in sorted(self.cache_data.keys()):
+            print("{}: {}".format(key, self.cache_data.get(key).data[1]))
 
     def put(self, key, item):
-        """Method to insert a key-value pair."""
+        """
+        method to add an item in the cache
+        """
         if key is None or item is None:
             return
 
-        self.cache_data[key] = item
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            print("DISCARD: {}".format(self.lru.popitem(False)[0]))
-            del self.cache_data[self.lru.popitem(False)[0]]
+        if key in self.cache_data:
+            node = self.cache_data[key]
+            self.order.erase(node)
 
-        try:
-            del self.lru[key]
-        except ValueError:
-            pass
+            self.order.insert_front((key, item))
+            self.cache_data[key] = self.order.front()
 
-        self.lru.append(key)
+            return
+
+        if len(self.cache_data) == BaseCaching.MAX_ITEMS:
+            lru = self.order.back()
+            lru_key = lru.data[0]
+            print("DISCARD: {}".format(lru_key))
+            del self.cache_data[lru_key]
+            self.order.pop_back()
+
+        self.order.insert_front((key, item))
+        self.cache_data[key] = self.order.front()
 
     def get(self, key):
-        """Method to get a value from the cache."""
-        return self.cache_data.get(key)
+        """
+        Get an item by key
+        """
+
+        if key not in self.cache_data:
+            return None
+
+        node = self.cache_data[key]
+        item = node.data[1]
+
+        self.order.erase(node)
+
+        self.order.insert_front((key, item))
+
+        self.cache_data[key] = self.order.front()
+
+        return item
