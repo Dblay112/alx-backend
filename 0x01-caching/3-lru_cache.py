243b8ee7cbdcac9b086fd9fe52c8d7a675c67d23
Bright Djogatse <125486553+Dblay112@users.noreply.github.com>
2024-04-03 21:47:46 +0000

    Update 3-lru_cache.py

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -10,25 +10,25 @@ class LRUCache(BaseCaching):
     def __init__(self):
         super().__init__()
         self.lru = OrderedDict()
-        
+    
     def put(self, key, item):
         """method to insert key"""
         if key is None or item is None:
-          return
+            return
         self.cache_data[key] = item
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-          print("DISCARD: {}".format(self.lru[0]))
-          del self.cache_data[self.lru[0]]
-          self.cache_data.popitem(last=False)
-
+            print("DISCARD: {}".format(self.lru[0]))
+            del self.cache_data[self.lru[0]]
+            self.cache_data.popitem(last=False)
+            
         try:
-          self.lru.remove(key)
-
+            self.lru.remove(key)
+        
         except ValueError:
-          pass
-
+            pass
+            
         self.lru.append(key)
-
+        
     def get(self,key):
-      """method to get key"""
-      return self.cache_data.get(key)
+        """method to get key"""
+        return self.cache_data.get(key)
