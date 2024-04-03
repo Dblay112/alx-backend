47d76006b985190de99871edea499713aaaf8fee
Bright Djogatse <125486553+Dblay112@users.noreply.github.com>
2024-04-03 14:00:54 +0000

    Update 0-basic_cache.py

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -6,10 +6,14 @@ BaseCaching = __import__('base_caching').BaseCaching
 class BasicCache(BaseCaching):
     """a class BasicCache that inherits from BaseCaching"""
     def put(self, key, item):
-        """method to add an item to the cache"""
+        """
+        Add the item in the cache
+        """
         if key and item:
             self.cache_data[key] = item
 
     def get(self, key):
-        """method to retrieve an item from the cache"""
-        return self.cache_data.get[key]
+        """
+        Get an item by key
+        """
+        return self.cache_data.get(key)
