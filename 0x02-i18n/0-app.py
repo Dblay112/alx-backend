bca860f21810f8625d82399093b2d8a5bd35afa6
Bright Selorm Djogatse <brightdjogatse@gmail.com>
2024-04-10 12:45:33 +0000

    fixed pep8 errors

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 """flask module"""
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=True)
+@app.route('/', methods=['GET'], strict_slashes=False)
 def index():
+    """
+    hello world
+    """
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
