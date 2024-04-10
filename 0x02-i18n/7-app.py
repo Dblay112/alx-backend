ecd3f00e8a1da72ace8437ec7c3a0d5f63ade744
Bright Selorm Djogatse <brightdjogatse@gmail.com>
2024-04-10 15:56:20 +0000

    7-app.py

--- 0x02-i18n/7-app.py
+++ 0x02-i18n/7-app.py
@@ -3,8 +3,13 @@
 Babel module
 """
 
+from crypt import methods
+from email import header
+import babel
 from flask import Flask, render_template, request, g
 from flask_babel import Babel
+import pytz
+import requests
 
 app = Flask(__name__)
 babel = Babel(app)
@@ -77,6 +82,28 @@ def get_locale():
     if headers:
         return headers
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@babel.timezoneselector
+def get_timezone():
+    """
+    get_timezone.
+    """
+    try:
+        timezone = request.args.get("timezone")
+        if timezone:
+            return pytz.timezone(timezone)
+        user = request.args.get("login_as")
+        if user:
+            timezone = users.get(int(user)).get('timezone')
+            if timezone:
+                return pytz.timezone(timezone)
+        timezone = request.headers.get("timezone")
+        if timezone:
+            return pytz.timezone(timezone)
+    except pytz.UnknownTimeZoneError:
+        return app.config.get('BABEL_DEFAULT_TIMEZONE')
+    return app.config.get('BABEL_DEFAULT_TIMEZONE')
 
 
 if __name__ == "__main__":
