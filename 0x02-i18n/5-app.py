4280f3356371deafc49f03237987761e1e07529a
Bright Selorm Djogatse <brightdjogatse@gmail.com>
2024-04-10 15:41:44 +0000

    added port

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -72,5 +72,5 @@ def get_locale():
     return request.accept_languages.best_match(["en", "fr"])
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
