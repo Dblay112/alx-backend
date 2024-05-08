d5ac3570059b002b19bb56c17d9443810b0b3465
Bright Selorm Djogatse <brightdjogatse@gmail.com>
2024-05-08 08:13:34 -0700

    Queuing System in JS

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,25 @@
+iimport redis from 'redis';
+
+const client = redis.createClient();
+
+client.on('connect', function() {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', function (error) {
+  console.error(`Redis client not connected to the server: ${error}`);
+});
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, redis.print);
+}
+
+function displaySchoolValue(schoolName) {
+  client.get(schoolName, function(err, reply) {
+    console.log(reply);
+  });
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
